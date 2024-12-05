d29487c9596f5763ff7bd24d1d6b465d78fba17b
David Saroka <ds2286@cornell.edu>
2024-12-05 04:27:17 +0000

    updated

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ extras['all'] = sum(extras.values(), [])
 
 setup(
     name='LoggingTools',
-    version='0.3.2', 
+    version='0.3.3', 
     description='This standardizes the logging for python applications.',
     author='David Saroka',  
     author_email='ds2286@cornell.edu',  
